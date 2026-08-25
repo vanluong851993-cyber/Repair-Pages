d7de055364265b776c0a2740334190c606911091
vanluong851993-cyber <vanluong851993@gmail.com>
2026-08-25 10:54:34 +0700

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -19,8 +19,7 @@ export const initialUsers: User[] = [
   ];
 export const InitialCustomers: Customer[] = [
   {
-    id: 'user-1',username: 'tên khách hàng', 'Nguyễn Văn A', phone: '000000000' },
-];
+   id: 'user_3', username: 'thkhachhang', name: 'Nguyễn Văn A', phone: '000000000' },
 export const initialProducts: Product[] = [
   {
     id: 'sp-001',
